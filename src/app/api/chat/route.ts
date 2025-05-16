3ad5e11c456cb131dbd2e4bca00788ea4e02cf5c
yalla_halim <madihalim@gmail.com>
2025-05-16 07:48:15 -0700

    Update route.ts
    Replaced the Open AI model

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -10,7 +10,7 @@ export async function POST(request: Request) {
     const { messages } = await request.json();
 
     const completion = await openai.chat.completions.create({
-      model: 'ft:gpt-3.5-turbo-0613:personal:ai-poet:88GTGA7b',
+      model: 'ft:gpt-4o-mini-2024-07-18:personal:adversarial-grandpa:BXVBIS13',
       messages,
       stream: false,
     });
